66288e4026d3b47dfed6d5f74f4a711507120694
timcastelijn <t.castelijn@hotmail.com>
2017-11-01 11:38:03 +0100

    updated to replace points with circles

--- checkAndExport.py
+++ checkAndExport.py
@@ -4,32 +4,36 @@ import scriptcontext as sc
 import math
 import time
 
-# V0.1
+# V0.3
 # to install as command (alias) or keyboard shortcut, use line below and change to your script file path
 
 # '_-runPythonScript "/Users/timcastelijn/Documents/programming/rhinoscript/checkAndExport.py"
 # '_-runPythonScript "E:/checkAndExport.py"
+# '_-runPythonScript "C:/checkAndExport.py"
 
 
 # function to reverse a curve if the normal points downwards in Z-dir
 # assumed is that only closed curves can have inside or outside 
 def setCurveDir(objs):
+    rs.UnselectAllObjects()
     count = 0
     for obj in objs:
-        # if rs.IsCurve(obj) and rs.IsCurvePlanar(obj):
         if rs.IsCurve(obj):
-            normal = rs.CurveNormal(obj)
+            if rs.IsCurveClosed(obj):
+                normal = rs.CurveNormal(obj)
+                if normal and normal[2] < 0:
+                    count += 1
+                    rs.ReverseCurve(obj)
+                    rs.SelectObject(obj)
 
-            if normal and normal[2] < 0:
-                count += 1
-                rs.ReverseCurve(obj)
-
-                # print "Curve {} flipped {}{}".format(obj, normal, normal2)
-
-    print "reversed " + str(count) + " curves"
+    rs.EnableRedraw(True)
+    rs.EnableRedraw(False)
+    
+    rs.MessageBox( "resvered curves  " + str(count) + " curves")
 
     # rs.SelectObjects(objs)
     # rs.Command("Dir")
+
 
 def isCurveOnCPlane(obj):
     if rs.IsCurvePlanar(obj):
@@ -37,12 +41,12 @@ def isCurveOnCPlane(obj):
         for pt in data:
             if(math.fabs( pt.Z )< 1e-8 ):
                 return True
-
+    
 	return False
 
 
 def isPointOnCplane(obj):
-    return (math.fabs( rs.PointCoordinates(obj)[2] ) < 1e-6)
+    return (math.fabs( rs.PointCoordinates(obj)[2] ) < 1e-8)
 
 def appendLayer(layers, obj):
     layer = rs.ObjectLayer(obj)
@@ -65,7 +69,6 @@ def checkCurvePosition(objs):
                 # print "Curve {} not on Cplane".format(obj)
                 selection.append(obj)
                 appendLayer(layers, obj)
-
         else:
             # must be point rs.IsPoint(obj):
             if not isPointOnCplane(obj):
@@ -83,6 +86,73 @@ def checkCurvePosition(objs):
     # when an object is found on > 0 layers, prompt for proceed
     if len(layers) > 0:
         msg = "there were non-planar or elevated curves found on layers:\n"
+        for layer in layers:
+            msg = msg + "- " + layer + " \n"
+
+        msg = msg + '\n Do you want to proceed?'
+
+        if rs.MessageBox(msg, 1) != 1:
+            # do not proceed with export
+            return False
+
+    # else
+    return True
+
+def diff(first, second):
+        second = set(second)
+        return [item for item in first if item not in second]
+    
+def checkCurveIntegrity(objs):
+
+    layers = []
+    selection = []
+    delete_objs = []
+    
+    for i, obj in enumerate(objs):
+        if rs.IsCurve(obj):
+        
+            # points = rs.CurveDiscontinuity(obj, 5)
+            # if points: 
+                # rs.AddPoints(points)
+                
+                # points2 = rs.CullDuplicatePoints(points)
+                # points3 = diff(points, points2)
+                # if points3: 
+                    # rs.AddPoints(points3)
+                    # return False
+            
+            
+            # check for disconnected endpoints
+            if not rs.IsCurveClosed(obj):
+                end = rs.CurveEndPoint(obj)
+                start = rs.CurveStartPoint(obj)
+                if rs.Distance(start, end)<0.1:
+                    #print "Curve {} not on Cplane".format(obj)
+                    selection.append(obj)
+                    appendLayer(layers, obj)
+            # check for too small curvelengths
+            # i=0
+            # while True:
+                # try:
+                    # leng = rs.CurveLength(obj,i)
+                    # if len < 0.02:
+                        # print len
+                        # selection.append(obj)
+                        # appendLayer(layers, obj)
+                        # break
+                # except:
+                    # break
+                # i+=1
+             
+    if len(selection) > 0:
+        rs.SelectObjects(selection)
+        
+    rs.EnableRedraw(True)
+    rs.EnableRedraw(False)
+    
+    # when an object is found on > 0 layers, prompt for proceed
+    if len(layers) > 0:
+        msg = "curves seem to be closed but are not:\n"
         for layer in layers:
             msg = msg + "- " + layer + " \n"
 
@@ -120,7 +190,7 @@ def explodeTextObjects(objs):
     
     for obj in objs:
         if rs.IsText(obj):
-            
+            # if ("CNC" in rs.ObjectLayer(obj)):
             # rs.GetBoolean(text, "get", True)
             # result = rs.TextObjectFont(obj, "MecSoft_Font-1")
             
@@ -155,8 +225,20 @@ def explodeBlock(objects):
 def filterObjects(objs):
     new_list = []
     for obj in objs:
-        if rs.IsCurve(obj) or rs.IsPoint(obj):
+        if rs.IsCurve(obj):
             new_list.append(obj)
+
+        elif rs.IsPoint(obj):
+            layer = rs.ObjectLayer(obj)
+            point=rs.coerce3dpoint(obj)
+
+            circle = rs.AddCircle(rs.WorldXYPlane(),3)
+            
+            rs.ObjectLayer(circle, layer)
+            rs.MoveObject(circle, [point.X, point.Y, point.Z])
+            new_list.append(circle)
+            rs.DeleteObject(obj)
+            # rs.DeleteObject(point)
         else:
             # remove from obj list
             rs.DeleteObject(obj)
@@ -191,30 +273,28 @@ def main():
     
     copies = filterObjects(copies)
      
-    # check curves for deviation from c-plane
-    proceed = checkCurvePosition(copies);
-
-	# if ok, start export
-    if proceed:
-        # rs.UnselectAllObjects()
-
-        simplify(copies)
-            
-        setCurveDir(copies);
-
-		# move to origin
-        result = moveToOrigin(copies);
-
-        if result:
-                    
-            # export
-            rs.SelectObjects(copies)
-            result = rs.Command("Export")
-            if result: print 'exported succesfully'
-            
-            return True
+    if checkCurveIntegrity(copies):
     
+        # check curves for deviation from c-plane
+        proceed = checkCurvePosition(copies);
 
+        # if ok, start export
+        if proceed:
+            # rs.UnselectAllObjects()
+
+            simplify(copies)
+                
+            setCurveDir(copies);
+
+            # move to origin
+            result = moveToOrigin(copies);
+
+            if result:
+                        
+                # export
+                rs.SelectObjects(copies)
+                result = rs.Command("Export")
+                if result: print 'exported succesfully'
             
     rs.DeleteObjects(copies)
     rs.EnableRedraw(True)
