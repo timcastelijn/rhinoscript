b4849e99cbd71eaad0b6de05a41e37db10f292bd
timcastelijn <t.castelijn@hotmail.com>
2017-11-16 18:00:57 +0100

    first commit

---
+++ checkSelfIntersection.py
@@ -0,0 +1,34 @@
+"""
+@name:          checkSelfIntersection
+@description:   checks curves for self intersection
+@author:        tim castelijn
+@version:       0.6
+@link:          https://github.com/timcastelijn/rhinoscript
+@notes:         Works with Rhino 5.
+"""
+
+
+import rhinoscriptsyntax as rs
+
+obj = rs.GetObject('select')
+
+def checkSelfIntersection(obj):
+    segments = rs.ExplodeCurves(obj, False)
+
+    list = segments
+    for seg in segments:
+        segA = seg
+        segments.remove(segA)
+        
+        for segB in segments:
+            intersection_list = rs.CurveCurveIntersection(segA, segB, tolerance=-1)
+            if intersection_list:
+                for intersection in intersection_list:
+                    if (intersection[0] == 2):
+                        rs.AddPoint(intersection[1])
+                        rs.AddPoint(intersection[2])
+
+    rs.DeleteObjects(list)
+
+
+checkSelfIntersection(obj)
